79ffb763b17a514f43c961ca19a7746b18d7e80a
Ivo Oliveira <ivo793@gmail.com>
2025-05-10 00:37:19 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// c4dc6ccccc8a9327028dddcb63cb0c76
+
+const toggleBtn = document.querySelector("#toggle");
+
+toggleBtn.addEventListener('click', () =>{
+    document.body.classList.toggle('dark')
+});
